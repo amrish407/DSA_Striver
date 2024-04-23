2ebd7df3b4084f6e9bff2f8bcb2d752ab3e9196c
amrish05 <amrishyadav407@gmail.com>
2024-04-23 22:03:58 +0530

    Learn the Basics (Lec - 4)

---
+++ Learn_The_Basics/Lec-4_Palindrome.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+bool palindrome(int n)
+{
+    // Write your code here
+    int m = n;
+    int reverse = 0;
+    while(m>0){
+        int digit = m%10;
+        reverse = reverse*10 + digit;
+        m = m/10;
+    }
+
+    if(reverse == n)
+        return true;
+    else
+        return false;
+}
