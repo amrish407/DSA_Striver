28ffaa1a22c7dc198e638af4e0211a18980c6d4c
amrish05 <amrishyadav407@gmail.com>
2024-05-14 12:06:44 +0530

    Insertion Sort (Lec - 1)

---
+++ Learn Important Sorting Techniques/Lec-1_Insertion-sort.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+
+//Code for (Geeks For Geeks) Platform
+//Problem -> Insertion Sort
+
+
+class Solution
+{
+    public:
+    void insert(int arr[], int i)
+    {
+        //code here
+    }
+     public:
+    //Function to sort the array using insertion sort algorithm.
+    void insertionSort(int arr[], int n)
+    {
+        //code here
+        for(int i=1; i<n; i++){
+            for(int j=0; j<i; j++){
+                if(arr[i]<arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+};
