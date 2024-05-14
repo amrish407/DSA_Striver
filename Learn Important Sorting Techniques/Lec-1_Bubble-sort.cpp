457ba3b42f7fd0c8aedd993d56f2dcede0423d84
amrish05 <amrishyadav407@gmail.com>
2024-05-14 11:54:08 +0530

    Bubble Sort (Lec -1)

---
+++ Learn Important Sorting Techniques/Lec-1_Bubble-sort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+
+//Code for the (Geeks For Geeks) Platform
+//Problem -> Bubble Sort
+
+
+class Solution {
+  public:
+    // Function to sort the array using bubble sort algorithm.
+    void bubbleSort(int arr[], int n) {
+        // Your code here
+        for(int i=n-1; i>=0; i--){
+            for(int j=0; j<i; j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+                
+            }
+        }
+    }
+};
