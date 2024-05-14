cb51ebe74a09c309363fd9022608df1a656e71a4
amrish05 <amrishyadav407@gmail.com>
2024-05-14 11:34:53 +0530

    Selection Sort (Lec - 1)

---
+++ Learn Important Sorting Techniques/Lec-1_Selection-sort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+
+//Code for the (Geeks For Geeks) Platform
+
+class Solution
+{
+    public:
+    int select(int arr[], int i)
+    {
+        // code here such that selectionSort() sorts arr[]
+    }
+     
+    void selectionSort(int arr[], int n)
+    {
+       //code here
+       for(int i=0; i<n; i++){
+           int min = i;
+           for(int j=i+1; j<n; j++){
+                if(arr[j]<arr[min])
+                    min = j;
+           }
+           int temp = arr[min];
+           arr[min] = arr[i];
+           arr[i] = temp;
+           
+       }
+    }
+};
