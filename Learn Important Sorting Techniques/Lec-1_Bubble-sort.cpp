ca1f0693e6fd1387234b132a790f0aae487a2677
amrish05 <amrishyadav407@gmail.com>
2024-05-21 21:25:31 +0530

    modified

--- Learn Important Sorting Techniques/Lec-1_Bubble-sort.cpp
+++ Learn Important Sorting Techniques/Lec-1_Bubble-sort.cpp
@@ -10,7 +10,7 @@ using namespace std;
 class Solution {
   public:
     // Function to sort the array using bubble sort algorithm.
-    void bubbleSort(int arr[], int n) {
+    void bubbleSort(int arr[], int num) {
         // Your code here
         for(int i=n-1; i>=0; i--){
             for(int j=0; j<i; j++){
